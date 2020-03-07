367290a99cf1ab3f5e659f689aed841cb1c67e14
weis999 <31774334+weis999@users.noreply.github.com>
2020-03-08 00:46:47 +0100

    Update bennoapp.py

--- bennoapp.py
+++ bennoapp.py
@@ -1,9 +1,6 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-  return 'Hey its Python Flask application!'
-
-if __name__ == '__main__':
-  app.run(host='0.0.0.0')
+@app.route("/")
+def index():
+    return "<h1>Hello Azure!</h1>"
