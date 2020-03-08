edf1f45939727b304132a921c6f57ad0d29c9a89
weis999 <31774334+weis999@users.noreply.github.com>
2020-03-08 01:22:24 +0100

    1

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route("/")
-def index():
-    return "<h1>Hello Azure!</h1>"
+def hello():
+    return "Hello World!"
+
+if __name__ == '__main__':
+    app.run()
