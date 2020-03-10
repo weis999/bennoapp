14b8622f264d7b8b7525d6c01c7ed7ddb0a941e7
weis999 <31774334+weis999@users.noreply.github.com>
2020-03-11 00:17:55 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
 from flask import Flask
-app = Flask(_name_)
+from flask_bcrypt import Bcrypt
+from flask_jwt_extended import JWTManager
+from resources.errors import errors
+from database.db import initialize_db
+from flask_restful import Api
+from resources.routes import initialize_routes
 
-@app.route('/')
-def hello_world():
-  return 'Hey its Python Flask application!'
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'nultjm2hoe0bioq32xohyt06bdrlp7'
+api = Api(app, errors=errors)
+bcrypt = Bcrypt(app)
+jwt = JWTManager(app)
+app.config['MONGODB_SETTINGS'] = {
+    'host': 'mongodb://weis999:68Y3bgxfbm12CmxMLVx1iPDndecXSzZFayQ8hY9NfOvfUq7Pey3HI2l6V0Y4IAca9WNIq11VZzxiqahJAnoITA==@weis999.mongo.cosmos.azure.com:10255/benno_01?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@weis999@'
+}
+initialize_db(app)
+initialize_routes(api)
 
-if _name_ == '_main_':
-  app.run(host='0.0.0.0')
-  
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
